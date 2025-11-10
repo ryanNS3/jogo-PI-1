61ebf37d529266993f4404736e1bfc47fddf0beb
ryansantosn333 <ryansantosn333@gmail.com>
2025-11-10 21:42:11 +0000

    feat/função de limpar console

---
+++ src/main/java/com/example/demo/utils/LimparConsole.java
@@ -0,0 +1,17 @@
+package com.example.demo.utils;
+
+public class LimparConsole {
+    public void limparConsole() {
+        try {
+            String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Não foi possível limpar o console.");
+        }
+    }
+    
+}
