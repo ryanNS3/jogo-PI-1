2078a5960b53476591757adeec4fb7d0d9bcf8c7
ryansantosn333 <ryansantosn333@gmail.com>
2025-11-10 21:41:53 +0000

    feat/criando classe main

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import com.example.demo.PartesHistoria.Cozinha;
+
+public class Main {
+    public static void main(String[] args) {
+        Cozinha teste = new Cozinha();
+
+        teste.Iniciar();
+    }
+}
